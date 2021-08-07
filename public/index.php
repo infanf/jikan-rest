5c093481ae00edc8db5b77ceda809781d608d66f
Christopher Schreiner <git@infanf.com>
2021-08-08 01:06:31 +0200

    feat: define CORS origin

--- public/index.php
+++ public/index.php
@@ -26,10 +26,12 @@ $app = require __DIR__.'/../bootstrap/app.php';
 
 ob_start("ob_gzhandler");
 
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: *");
 if (!env('APP_DEBUG')) {
-    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: " . env('APP_CORS'));
+} else {
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Methods: *");
 }
 
 $app->run();
